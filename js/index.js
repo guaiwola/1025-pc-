88eeae59ceb923f3c635f24dba1e11679295a9ed
chenxuan <guaiwola@qq.com>
2019-01-04 18:25:02 +0800

    修改bug

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-//等待页面加载（所有资源  图片、音、视频等资源）完成，才会调用此函数
+
 window.addEventListener('DOMContentLoaded', function () {
     //获取dom元素
     var headerLisNodes = document.querySelectorAll('.nav li');
@@ -8,21 +8,20 @@ window.addEventListener('DOMContentLoaded', function () {
     var contentNode = document.querySelector('.content');
 
     var contentHeight = contentNode.offsetHeight;
+    var arrowHalfWidth = arrowNode.offsetWidth / 2;
     var nowIndex = 0;
+    var wheelTimer = null;
 
     //处理头部js代码
     headerHandle();
     function headerHandle() {
-
-        //初始化时小箭头来到第一个li下面
         arrowNode.style.left = headerLisNodes[0].getBoundingClientRect().left + headerLisNodes[0].offsetWidth / 2
-            - arrowNode.offsetWidth / 2 + 'px';
+            - arrowHalfWidth + 'px';
         headerDownNodes[0].style.width = '100%';
 
         for (var i = 0; i < headerLisNodes.length; i++) {
             headerLisNodes[i].index = i;
             headerLisNodes[i].onclick = function () {
-                //同步更新nowIndex的值，否则点击后在滚动就会出bug
                 nowIndex = this.index;
                 move(nowIndex);
             }
@@ -31,16 +30,12 @@ window.addEventListener('DOMContentLoaded', function () {
 
     //公共move函数
     function move(nowIndex) {
-        //默认清空所有width为0
         for (var j = 0; j < headerDownNodes.length; j++) {
             headerDownNodes[j].style.width = '';
         }
-        //设置当前width为100%
         headerDownNodes[nowIndex].style.width = '100%';
-        //让小箭头去当前点击的li的下面
         arrowNode.style.left = headerLisNodes[nowIndex].getBoundingClientRect().left + headerLisNodes[nowIndex].offsetWidth / 2
-            - arrowNode.offsetWidth / 2 + 'px';
-        //让内容区ul运动
+            - arrowHalfWidth + 'px';
         contentUlNode.style.top = - nowIndex * contentHeight + 'px';
     }
 
@@ -53,44 +48,50 @@ window.addEventListener('DOMContentLoaded', function () {
 
         function wheel(event) {
             event = event || window.event;
-
-            var flag = '';
-            if (event.wheelDelta) {
-                //ie/chrome
-                if (event.wheelDelta > 0) {
-                    flag = 'up';
-                } else {
-                    flag = 'down'
-                }
-            } else if (event.detail) {
-                //firefox
-                if (event.detail < 0) {
-                    flag = 'up';
-                } else {
-                    flag = 'down'
-                }
-            }
-
-            switch (flag) {
-                case 'up' :
-                    if (nowIndex > 0) {
-                        nowIndex--;
-                        move(nowIndex);
+            clearTimeout(wheelTimer);
+            wheelTimer = setTimeout(function () {
+                var flag = '';
+                if (event.wheelDelta) {
+                    if (event.wheelDelta > 0) {
+                        flag = 'up';
+                    } else {
+                        flag = 'down';
                     }
-                    break;
-                case 'down' :
-                    if (nowIndex < 4) {
-                        nowIndex++;
-                        move(nowIndex);
+                } else if (event.detail) {
+                    //firefox
+                    if (event.detail < 0) {
+                        flag = 'up';
+                    } else {
+                        flag = 'down';
                     }
-                    break;
-            }
+                }
 
-            //禁止默认行为
+                switch (flag) {
+                    case 'up' :
+                        if (nowIndex > 0) {
+                            nowIndex--;
+                            move(nowIndex);
+                        }
+                        break;
+                    case 'down' :
+                        if (nowIndex < 4) {
+                            nowIndex++;
+                            move(nowIndex);
+                        }
+                        break;
+                }
+
+            }, 200);
             event.preventDefault && event.preventDefault();
             return false;
         }
+    }
 
+    //浏览器调整窗口大小事件
+    window.onresize = function () {
+        arrowNode.style.left = headerLisNodes[nowIndex].getBoundingClientRect().left + headerLisNodes[nowIndex].offsetWidth / 2
+            - arrowHalfWidth + 'px';
+        contentUlNode.style.top = - nowIndex * contentHeight + 'px';
     }
 
 })
